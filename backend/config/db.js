eb679a485a73d6a669682869e059648fe4041838
Mohd-Salman-0119 <salmanansari910550@gmail.com>
2023-12-22 22:48:38 +0530

    add: MONGO_URL Key

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(`${process.env.MONGODB_URL}/mobile-ordering-app`);
+const connection = mongoose.connect(`${process.env.MONGO_URI}/mobile-ordering-app`);
 
 module.exports = { connection }
